34edd93814460161a76c7deb83ff92bde2271c77
Simone <simonepapa24@gmail.com>
2025-01-24 18:12:41 +0100

    Changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,4 @@
-import Navbar from "../components/ui/Navbar"
-import ThemeToggler from "../components/ui/ThemeToggler"
-
 function Home() {
-  return (
-    <div>
-      <Navbar />
-      <div className="mt-4 hidden xl:block">
-        <ThemeToggler />
-      </div>
-    </div>
-  )
+  return <div>test</div>
 }
 export default Home
